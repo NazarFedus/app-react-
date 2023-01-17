40f986ed9cce76bd00417b2b10134315a8255b84
NazarFedus <simpleprograming7@gmail.com>
2023-01-17 11:13:28 +0000

    Downloading data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import {Product} from './components/Product'
-import {products} from './data/products'
+// import {products} from './data/products'
+import { IProduct } from './models';
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  async function fetchProducts(){
+    try{
+      setError('')
+      setLoading(true)
+      const reponse = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
+      setProducts(reponse.data)
+      setLoading(false)
+    } catch (e: unknown){
+      const error = e as Error
+      setLoading(false)
+      setError(error.message)
+    }
+
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product = {products[0]}></Product>
-      <Product product = {products[1]}></Product>
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
 
+    {products.map(product => <Product product = {product} key={product.id}></Product>)}
     </div>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IProduct } from '../models'
 
 interface ProductProps {
@@ -6,8 +6,22 @@ interface ProductProps {
 }
 
 export function Product(props: ProductProps){
+     const [details, setDetails] = useState(false)
+
+     const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+     const btnClasses = ['py-2 px-4 border', btnBgClassName].join(' ')
+
      return (
-          <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>{props.product.title}</div>
+          <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
+               <img src={props.product.image} className = "w-1/6" alt={props.product.title}></img>
+               <p>{props.product.title}</p>
+               <p className='font-bold'>{props.product.price}</p>
+               <button className = {btnClasses} onClick={() => setDetails(prev => !prev)}>{details ? 'Hide Details' : 'Show Details'}</button>
+               {details && <div>
+                    <p>{props.product.description}</p>
+                    <p>Rate: <span style={{fontWeight: 'bold'}}>{props.product.rating.rate}</span></p>
+               </div>}
+          </div>
 
      )
 }
