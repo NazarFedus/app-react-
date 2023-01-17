b9b8bccab23fba5aa1551492e7056657c3108eb4
NazarFedus <simpleprograming7@gmail.com>
2023-01-17 11:40:16 +0000

    create 2 components (loading, errorMessage)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import {Product} from './components/Product'
+import { Loader } from './components/Loader';
 import { useProducts } from './hooks/products';
+import { ErrorMessage } from './components/ErrorMessage';
 
 
 
@@ -8,8 +10,8 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      { loading && <p className="text-center">Loading...</p> }
-      { error && <p className="text-center text-red-600">{error}</p> }
+      { loading &&  <Loader></Loader> }
+      { error && <ErrorMessage error = {error}></ErrorMessage> }
       { products.map(product => <Product product = {product} key={product.id}></Product>) }
     </div>
   )
