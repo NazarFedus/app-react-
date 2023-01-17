df68ce55afa750326f9d43e57c6c06ea3aae88a0
NazarFedus <simpleprograming7@gmail.com>
2023-01-17 11:25:21 +0000

    separate file for request

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 import {Product} from './components/Product'
-// import {products} from './data/products'
-import { IProduct } from './models';
+import { useProducts } from './hooks/products';
+
+
 
 function App() {
-
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts(){
-    try{
-      setError('')
-      setLoading(true)
-      const reponse = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
-      setProducts(reponse.data)
-      setLoading(false)
-    } catch (e: unknown){
-      const error = e as Error
-      setLoading(false)
-      setError(error.message)
-    }
-
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const {products, loading, error} = useProducts()
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className="text-center">Loading...</p>}
-      {error && <p className="text-center text-red-600">{error}</p>}
-
-    {products.map(product => <Product product = {product} key={product.id}></Product>)}
+      { loading && <p className="text-center">Loading...</p> }
+      { error && <p className="text-center text-red-600">{error}</p> }
+      { products.map(product => <Product product = {product} key={product.id}></Product>) }
     </div>
   )
 }
---
+++ src/hooks/products.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import { IProduct } from '../models'
+import axios from 'axios'
+
+
+export function useProducts(){
+     const [products, setProducts] = useState<IProduct[]>([])
+     const [loading, setLoading] = useState(false)
+     const [error, setError] = useState('')
+
+     async function fetchProducts(){
+       try{
+         setError('')
+         setLoading(true)
+         const reponse = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7')
+         setProducts(reponse.data)
+         setLoading(false)
+       } catch (e: unknown){
+         const error = e as Error
+         setLoading(false)
+         setError(error.message)
+       }
+
+     }
+
+     useEffect(() => {
+       fetchProducts()
+     }, [])
+
+     return {products, loading, error}
+}
