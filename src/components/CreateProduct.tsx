2d1a51f0bba1ba8b60eaa4581b98aec53e1bdc1c
NazarFedus <simpleprograming7@gmail.com>
2023-01-18 13:30:36 +0000

    send data on sever

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
+import { IProduct } from '../models'
+import axios from "axios";
+import { ErrorMessage } from "./ErrorMessage";
+
+
+
+const productData = {
+     title: '',
+     price: 13.5,
+     description: 'lorem ipsum set',
+     image: 'https://i.pravatar.cc',
+     category: 'electronic',
+     rating: {
+          rate: 4.5,
+          count: 10
+     }
+}
 
 export function CreateProduct() {
 
      const [value, setValue] = useState('')
+     const [error, setError] = useState('')
 
-     const submitHandler = (event: React.FormEvent) => {
+     const submitHandler = async (event: React.FormEvent) => {
           event.preventDefault()
+          setError('')
+
+          if(value.trim() === ''){
+               setError('Please enter valid title...')
+               return
+          }
+          productData.title = value
+          const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
      }
 
      const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -14,13 +40,17 @@ export function CreateProduct() {
 
      return (
           <form onSubmit={submitHandler}>
-               <input type="text" className="border py-2 px-4 mb-2 w-full outline-0"
-               placeholder="Enter product title..."
-               value = {value}
-               onChange = {changeHandler}
-               />
-               <button type="submit" className="py-2 px-4 border bg-green-400 hover:text-white"
-               >Create</button>
-          </form>
-     )
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
+      />
+
+      {error && <ErrorMessage error={error} />}
+
+      <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
+    </form>
+  )
 }
