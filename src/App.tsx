5eb3e1e3ab1a0ba2ab233142c581e0a68e3f6cd4
NazarFedus <simpleprograming7@gmail.com>
2023-01-18 14:30:00 +0000

    clear some code by context

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import { useProducts } from './hooks/products';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Modal } from './components/Modal';
 import { CreateProduct } from './components/CreateProduct';
-import { useState } from 'react';
+import { useContext} from 'react';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 
 
 function App() {
   const {products, loading, error, addProduct} = useProducts()
-  const [modal, setModal] = useState(false)
+  const {modal, open, close} = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    close()
     addProduct(product)
   }
 
@@ -24,12 +25,12 @@ function App() {
       { error && <ErrorMessage error = {error}></ErrorMessage> }
       { products.map(product => <Product product = {product} key={product.id}></Product>) }
 
-    {modal && <Modal title='Create new product' onClose={() => setModal(false)}>
+    {modal && <Modal title='Create new product' onClose={close}>
       <CreateProduct onCreate={createHandler} ></CreateProduct>
     </Modal>}
 
     <button className='fixed bottom-5 right-5 rounded-full bg-green-700 text-white text-2xl px-5 py-3'
-    onClick={()=>setModal(true)}
+    onClick={open}
     >+</button>
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ModalState } from './context/ModalContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
-root.render(<App />)
+root.render(
+  <ModalState>
+    <App />
+  </ModalState>
+
+)
