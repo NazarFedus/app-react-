6f660f21c5397cd9e4e793a317cbd8339eb808db
NazarFedus <simpleprograming7@gmail.com>
2023-02-13 20:22:15 +0000

    add structure

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Basket } from "./Basket__component/Basket";
-import { Registration } from "./Registration";
+
 
 
 
 
 export function Navigation() {
-
-     const [currentStatus, setCurrentStatus] = useState(false)
-
-     useEffect(() => {
-          console.log(currentStatus)
-     }, [currentStatus])
 
      return (
           <nav className="w-[100%] h-[50px] flex justify-between items-center px-5 bg-gray-500 text-white">
@@ -21,11 +15,11 @@ export function Navigation() {
                     <Link to="/" className="mr-2">Products</Link>
                     <Link to="/about">About</Link>
                     <Basket></Basket>
-                    <button
+                    {/* <button
                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                          onClick={() => setCurrentStatus(prev => !prev)}>{currentStatus ? 'LogOut' : 'LogIn'}
-                    </button>
-                    {/* {currentStatus && <Registration></Registration>} */}
+                    </button> */}
+                    <Link to="/registration">Register/LogIn</Link>
                </span>
 
           </nav>
