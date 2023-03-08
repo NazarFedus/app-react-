ce85ab7fb06367d368d01e0ecd8825fa4de5c098
NazarFedus <simpleprograming7@gmail.com>
2023-03-08 12:32:06 +0000

    add registration form

--- src/components/Register&LogIn/RegisterForm.tsx
+++ src/components/Register&LogIn/RegisterForm.tsx
@@ -1,35 +1,30 @@
 import React, {useState, useRef} from "react";
 import {useSubmit} from "../../hooks/useSubmit";
-import {useFormik} from "formik";
+import {Formik} from "formik";
 import * as Yup from "yup";
+import { errorMonitor } from "events";
+
+const error = {
+     color: 'red',
+     fontSize: '15px',
+     margin: '0',
+     padding: '0',
+}
 
 export function RegisterForm() {
-     const [valid, setValid] = useState(true)
-     const {submit} = useSubmit()
 
-     function clickHandler(e: React.MouseEvent<HTMLButtonElement>){
-          e.preventDefault()
-          alert('You are registered')
-     }
-     function submitHandler(e: React.FormEvent<HTMLFormElement>){
-          e.preventDefault()
-          formik.submitForm()
-          console.log(formik.values)
-          submit(formik.values)
-     }
-
-
-     const formik = useFormik({
-          initialValues: {
-               name: '',
-               email: '',
-               password: '',
-               confirmPassword: ''
-          },
-          onSubmit: values => {
-               submit(values)
-          },
-          validationSchema: Yup.object({
+return (
+     <div>
+          <Formik
+               initialValues = {{
+                    name: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+               }}
+               validateOnBlur
+               onSubmit = { values => {console.log(values)}}
+               validationSchema = {Yup.object().shape({
                name: Yup.string().required('Required'),
                email: Yup.string().email('Invalid email address').required('Required'),
                password: Yup.string()
@@ -38,27 +33,72 @@ export function RegisterForm() {
                confirmPassword: Yup.string()
                          .oneOf([Yup.ref('password')], 'Passwords must match')
                          .required('Required')
-          }),
-          })
-
-
-     return (
-          <div>
-               <div>
-               <form onSubmit={submitHandler} className='form'>
+          })}
+          >
+               {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty}) => (
+                    <form className='form'>
                     <h1 className="form__title">Register</h1>
-                    <input type="text" name="name" onChange={formik.handleChange} placeholder="Name" className="form__input"></input>
-                    <input type="text" placeholder="Email" className="form__input"></input>
-                    <input
-                         type="password"
-                         placeholder="Password"
-                         // onChange={changeHandler}
-                         className="form__input">
-                    </input>
-                    <input type="text" placeholder="Confirm Password" className="form__input" disabled={valid}></input>
-                    <button onClick={clickHandler} className='form__button'>Register</button>
+                         <p>
+                              <label htmlFor="name">Name</label><br/>
+                              <input
+                                   type="text"
+                                   name="name"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.name}
+                                   className="form__input"
+                                   placeholder="Name" >
+                              </input>
+                         </p>
+                    { touched.name && errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
+
+                         <p>
+                              <label htmlFor="email">Email</label><br/>
+                              <input
+                                   type="text"
+                                   name="email"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.email}
+                                   className="form__input"
+                                   placeholder="Email" >
+                              </input>
+                         </p>
+                    { touched.email && errors.email && <p style={error}>{errors.email}</p>}
+
+                         <p>
+                              <label htmlFor="password">Password</label><br/>
+                              <input
+                                   type="password"
+                                   name="password"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.password}
+                                   className="form__input"
+                                   placeholder="Password" >
+                              </input>
+                         </p>
+                    { touched.password && errors.password && <p style={{color: 'red'}}>{errors.password}</p>}
+
+                         <p>
+                              <label htmlFor="confirmPassword">Confirm Password</label><br/>
+                              <input
+                                   type="password"
+                                   name="confirmPassword"
+                                   onChange={handleChange}
+                                   onBlur={handleBlur}
+                                   value={values.password}
+                                   className="form__input"
+                                   placeholder="Confirm password" >
+                              </input>
+                         </p>
+                    { touched.confirmPassword && errors.confirmPassword && <p style={{color: 'red'}}>{errors.confirmPassword}</p>}
+
+
+                    <button type='submit' disabled={!isValid && !dirty} className='form__button'>Register</button>
                </form>
-          </div>
-          </div>
+          )}
+          </Formik>
+     </div>
      )
 }
